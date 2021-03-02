2a6fe6963c5d37303670eb0c99b09865d738c3dc
Stelios-Ervis-Priftis <steliospriftis@gmail.com>
2021-03-02 21:17:51 +0100

    Styling footer, icons been added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,6 +4,9 @@ import { graphql, useStaticQuery } from "gatsby"
 // Footer Styles SCSS Import
 import footerStyles from "./footer.module.scss"
 
+// React Icons Import
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa"
+
 const Footer = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -16,9 +19,24 @@ const Footer = () => {
   `)
   return (
     <div className={footerStyles.container}>
-      <p>
-        CREATED BY {data.site.siteMetadata.author} ©{new Date().getFullYear()}
-      </p>
+      <div className={footerStyles.icons}>
+        <a href="https://github.com/Stelios-Ervis-Priftis" target="#">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/ervispriftis/" target="#">
+          <FaLinkedin />
+        </a>
+
+        <a href="https://www.instagram.com/stelios_priftis/" target="#">
+          <FaInstagram />
+        </a>
+      </div>
+
+      <div className={footerStyles.copyright}>
+        <p>
+          CREATED BY {data.site.siteMetadata.author} ©{new Date().getFullYear()}
+        </p>
+      </div>
     </div>
   )
 }
