2ec4fdb8add259de117f7d82ef91a160f05b9f70
Stelios-Ervis-Priftis <steliospriftis@gmail.com>
2021-03-26 18:28:33 +0100

    Contact form testing

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 // Components Import
 import Layout from "../../components/layout/layout"
@@ -15,108 +15,137 @@ import { ImMail4 } from "react-icons/im"
 import { RiSendPlaneFill } from "react-icons/ri"
 import { GiBroom } from "react-icons/gi"
 
-const Contact_Page = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <Container fluid className={contactStyles.contactContainer}>
-      <Row className={contactStyles.contactRow}>
-        <Col lg={6} className={contactStyles.iconContainer}>
-          <ImMail4 />
-        </Col>
+const Contact_Page = () => {
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    textArea: "",
+  })
 
-        <Col lg={6}>
-          <Form
-            className={contactStyles.formContainer}
-            name="contact"
-            action="POST"
-            data-netlify="true"
-          >
-            <h1>Contact Me</h1>
-            <Row>
-              <Col lg={6}>
-                <Form.Group controlId="formBasicFirstName">
-                  <Form.Control
-                    type="text"
-                    name="first-name"
-                    required
-                    placeholder="First Name"
-                  />
-                </Form.Group>
-              </Col>
-              <Col lg={6}>
-                <Form.Group controlId="formBasicLastName">
-                  <Form.Control
-                    type="text"
-                    name="last-name"
-                    required
-                    placeholder="Last Name"
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control
-                type="email"
-                name="email"
-                required
-                placeholder="Email Address"
-              />
-            </Form.Group>
+  const encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
 
-            <Form.Group controlId="formBasicSubject">
-              <Form.Control
-                type="text"
-                name="subject"
-                required
-                placeholder="Subject"
-              />
-            </Form.Group>
+  const handleChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    })
+    console.log("e", e)
+  }
 
-            <Form.Group controlId="formBasicTextBody">
-              <Form.Control
-                as="textarea"
-                name="text-area"
-                required
-                rows="4"
-                style={{ resize: "none" }}
-                placeholder="Message ..."
-              />
-            </Form.Group>
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formState }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
 
-            <div className={contactStyles.ctaContainer}>
-              <Button className={contactStyles.ctaSend} type="submit">
-                SEND <RiSendPlaneFill />
-              </Button>
+    e.preventDefault()
+  }
 
-              <Button className={contactStyles.ctaClear} type="reset">
-                CLEAR <GiBroom />
-              </Button>
-            </div>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-    {/* <form method="post" action="#">
-      <label>
-        Name
-        <input type="text" name="name" id="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" id="email" />
-      </label>
-      <label>
-        Subject
-        <input type="text" name="subject" id="subject" />
-      </label>
-      <label>
-        Message
-        <textarea name="message" id="message" rows="5" />
-      </label>
-      <button type="submit">Send</button>
-      <input type="reset" value="Clear" />
-    </form> */}
-  </Layout>
-)
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <Container fluid className={contactStyles.contactContainer}>
+        <Row className={contactStyles.contactRow}>
+          <Col lg={6} className={contactStyles.iconContainer}>
+            <ImMail4 />
+          </Col>
+
+          <Col lg={6}>
+            <Form
+              className={contactStyles.formContainer}
+              onSubmit={handleSubmit}
+              name="contact"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <h1>Contact Me</h1>
+              <Row>
+                <Col lg={6}>
+                  <Form.Group controlId="formBasicFirstName">
+                    <Form.Control
+                      type="text"
+                      name="firstName"
+                      onChange={handleChange}
+                      value={formState.firstName}
+                      placeholder="First Name"
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+                <Col lg={6}>
+                  <Form.Group controlId="formBasicLastName">
+                    <Form.Control
+                      type="text"
+                      name="lastName"
+                      onChange={handleChange}
+                      value={formState.lastName}
+                      placeholder="Last Name"
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={formState.email}
+                  placeholder="Email Address"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicSubject">
+                <Form.Control
+                  type="text"
+                  name="subject"
+                  onChange={handleChange}
+                  value={formState.subject}
+                  placeholder="Subject"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicTextBody">
+                <Form.Control
+                  as="textarea"
+                  name="textA  rea"
+                  onChange={handleChange}
+                  value={formState.textArea}
+                  placeholder="Message ..."
+                  rows="4"
+                  style={{ resize: "none" }}
+                  required
+                />
+              </Form.Group>
+
+              <div className={contactStyles.ctaContainer}>
+                <Button className={contactStyles.ctaSend} type="submit">
+                  SEND <RiSendPlaneFill />
+                </Button>
+
+                <Button className={contactStyles.ctaClear} type="reset">
+                  CLEAR <GiBroom />
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  )
+}
 
 export default Contact_Page
