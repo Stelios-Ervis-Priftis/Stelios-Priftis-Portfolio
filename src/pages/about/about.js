d5423461f74cdf07c4493ef4eac212d25ace3582
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-08-28 10:15:22 +0200

    Update texts

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,115 +1,132 @@
 import React from 'react';
 
 // Components Import
+import Image from '../../components/image';
 import Layout from '../../components/layout/layout';
 import SEO from '../../components/seo';
-import Image from '../../components/image';
 
 // React Bootstrap Import
 import { Container } from 'react-bootstrap';
 
 // Style Import
-import aboutStyles from './about.module.scss';
 import 'react-sweet-progress/lib/style.css';
+import aboutStyles from './about.module.scss';
 
 const About_Page = () => {
-  const imageSize = {
-    width: '120px',
-    height: '120px',
-  };
+	const imageSize = {
+		width: '120px',
+		height: '120px',
+	};
 
-  return (
-    <Layout>
-      <SEO title="About" />
-      <Container className={aboutStyles.aboutWrapper}>
-        <Container className={aboutStyles.aboutSectionsContainer}>
-          <Container className={aboutStyles.aboutContainer}>
-            <h4>About me and my hobbies</h4>
+	return (
+		<Layout>
+			<SEO title="About" />
+			<Container className={aboutStyles.aboutWrapper}>
+				<Container className={aboutStyles.aboutSectionsContainer}>
+					<Container className={aboutStyles.aboutContainer}>
+						<h4>About me and my hobbies</h4>
 
-            <p> I’m a Software Engineer with a background in Sound Engineering.</p>
-            <p>
-              My curiosity about software programs began while I was working as a Sound Engineer. Fascinated
-              by how these programs function, I started exploring the world of coding. This journey led me to
-              discover a wealth of courses and information about programming and its vast potential.
-            </p>
-            <p>
-              After experimenting with code in my spare time, I realized I was hooked. To further my knowledge
-              and skills, I enrolled in a BootCamp course, which provided comprehensive training in full-stack
-              technologies. This experience allowed me to work on several projects, expanding my passion for
-              software development.
-            </p>
-            <p>
-              Now, as a dedicated Software Engineer, I bring a unique perspective from my sound engineering
-              background, combining creativity and technical expertise to deliver innovative solutions. I'm
-              proficient in technologies such as JavaScript and frameworks like React. My experience showcases
-              my ability to develop and deploy robust applications.
-            </p>
-            <p>
-              I am excited to continue growing in this field, take advantage of my problem-solving skills,
-              creativity, and collaborative nature to contribute to meaningful projects. My career aspiration
-              is to become an expert in software engineering, continually learning and adapting to new
-              technologies to create meaningful and innovative solutions.
-            </p>
-          </Container>
+						<p>
+							I’m a Software Developer with a background in Sound
+							Engineering.
+						</p>
+						<p>
+							My curiosity about software programs began while I was
+							working as a Sound Engineer. Fascinated by how these
+							programs function, I started exploring the world of coding.
+							This journey led me to discover a wealth of courses and
+							information about programming and its vast potential.
+						</p>
+						<p>
+							After experimenting with code in my spare time, I realized
+							I was hooked. To further my knowledge and skills, I
+							enrolled in a BootCamp course, which provided comprehensive
+							training in full-stack technologies. This experience
+							allowed me to work on several projects, expanding my
+							passion for software development.
+						</p>
+						<p>
+							Now, as a dedicated Software Engineer, I bring a unique
+							perspective from my sound engineering background, combining
+							creativity and technical expertise to deliver innovative
+							solutions. I'm proficient in technologies such as
+							JavaScript and frameworks like React. My experience
+							showcases my ability to develop and deploy robust
+							applications.
+						</p>
+						<p>
+							I am excited to continue growing in this field, take
+							advantage of my problem-solving skills, creativity, and
+							collaborative nature to contribute to meaningful projects.
+							My career aspiration is to become an expert in software
+							engineering, continually learning and adapting to new
+							technologies to create meaningful and innovative solutions.
+						</p>
+					</Container>
 
-          <Container className={aboutStyles.hobbiesContainer}>
-            <div className={aboutStyles.windsurfingContainer}>
-              <Image
-                fileName="windsurfing.jpg"
-                className={`${aboutStyles.windsurfingHero} rounded-circle`}
-                style={imageSize}
-                alt="Hero of the pic"
-              />
+					<Container className={aboutStyles.hobbiesContainer}>
+						<div className={aboutStyles.windsurfingContainer}>
+							<Image
+								fileName="windsurfing.jpg"
+								className={`${aboutStyles.windsurfingHero} rounded-circle`}
+								style={imageSize}
+								alt="Hero of the pic"
+							/>
 
-              <div className={aboutStyles.windsurfingBio}>
-                <h4>Wind Surfing</h4>
-                <p>
-                  Windsurfing is my favorite summer activity. The joy of riding on the water, driven by the
-                  wind, is unlike anything else. It’s about learning to control the elements, challenging
-                  myself, and enjoying nature.
-                </p>
-              </div>
-            </div>
+							<div className={aboutStyles.windsurfingBio}>
+								<h4>Wind Surfing</h4>
+								<p>
+									Windsurfing is my favorite summer activity. The joy
+									of riding on the water, driven by the wind, is unlike
+									anything else. It’s about learning to control the
+									elements, challenging myself, and enjoying nature.
+								</p>
+							</div>
+						</div>
 
-            <div className={aboutStyles.snowboarderContainer}>
-              <Image
-                fileName="snowboarding.jpg"
-                className={`${aboutStyles.snowboarderHero} rounded-circle`}
-                style={imageSize}
-                alt="Hero of the pic"
-              />
+						<div className={aboutStyles.snowboarderContainer}>
+							<Image
+								fileName="snowboarding.jpg"
+								className={`${aboutStyles.snowboarderHero} rounded-circle`}
+								style={imageSize}
+								alt="Hero of the pic"
+							/>
 
-              <div className={aboutStyles.snowboarderBio}>
-                <h4>Snow Boarding</h4>
-                <p>
-                  Snowboarding is my winter passion. The joy of sliding down fresh slopes in a snowy landscape
-                  is unmatched. It's a sport that blends excitement and creativity.
-                </p>
-              </div>
-            </div>
+							<div className={aboutStyles.snowboarderBio}>
+								<h4>Snow Boarding</h4>
+								<p>
+									Snowboarding is my winter passion. The joy of sliding
+									down fresh slopes in a snowy landscape is unmatched.
+									It's a sport that blends excitement and creativity.
+								</p>
+							</div>
+						</div>
 
-            <div className={aboutStyles.bartendingContainer}>
-              <Image
-                fileName="bartending.jpg"
-                className={`${aboutStyles.bartendingHero} rounded-circle`}
-                style={imageSize}
-                alt="Hero of the pic"
-              />
+						<div className={aboutStyles.bartendingContainer}>
+							<Image
+								fileName="bartending.jpg"
+								className={`${aboutStyles.bartendingHero} rounded-circle`}
+								style={imageSize}
+								alt="Hero of the pic"
+							/>
 
-              <div className={aboutStyles.bartendingBio}>
-                <h4>Mixology</h4>
-                <p>
-                  Mixology is my exciting hobby, a mix of art and flavors. Creating cocktails is like making a
-                  symphony, with each ingredient adding to the taste. It's a creative activity that lets me
-                  experiment, innovate, and surprise, whether making classic drinks or inventing new ones.
-                </p>
-              </div>
-            </div>
-          </Container>
-        </Container>
-      </Container>
-    </Layout>
-  );
+							<div className={aboutStyles.bartendingBio}>
+								<h4>Mixology</h4>
+								<p>
+									Mixology is my exciting hobby, a mix of art and
+									flavors. Creating cocktails is like making a
+									symphony, with each ingredient adding to the taste.
+									It's a creative activity that lets me experiment,
+									innovate, and surprise, whether making classic drinks
+									or inventing new ones.
+								</p>
+							</div>
+						</div>
+					</Container>
+				</Container>
+			</Container>
+		</Layout>
+	);
 };
 
 export default About_Page;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
 import { Link } from 'gatsby';
+import React from 'react';
 
 // Component Import
-import SEO from '../../components/seo';
-import Layout from '../../components/layout/layout';
 import Image from '../../components/image';
+import Layout from '../../components/layout/layout';
+import SEO from '../../components/seo';
 
 // React Bootstrap Import
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 // Style Import
 import indexStyles from './index.module.scss';
@@ -16,41 +16,42 @@ import indexStyles from './index.module.scss';
 import { BiMailSend } from 'react-icons/bi';
 
 const Index_Page = () => {
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <Container fluid className={indexStyles.indexContainer}>
-        <Row className={indexStyles.indexRow}>
-          <Col lg="6" className={indexStyles.heroCol}>
-            <Image
-              fileName="sep.jpeg"
-              className={`${indexStyles.heroImg} rounded-circle`}
-              style={{ width: '370px' }}
-              alt="Hero of the pic"
-            />
-          </Col>
+	return (
+		<Layout>
+			<SEO title="Home" />
+			<Container fluid className={indexStyles.indexContainer}>
+				<Row className={indexStyles.indexRow}>
+					<Col lg="6" className={indexStyles.heroCol}>
+						<Image
+							fileName="sep.jpeg"
+							className={`${indexStyles.heroImg} rounded-circle`}
+							style={{ width: '370px' }}
+							alt="Hero of the pic"
+						/>
+					</Col>
 
-          <Col lg="6" className={indexStyles.bioCol}>
-            <div className={indexStyles.heroBio}>
-              <h1>Hello, I'm Stelios</h1>
-              <p>
-                A motivated and reliable Software Engineer seeking to work and grow in a tech environment.
-              </p>
-              <Link to="/contact">
-                <Button className={indexStyles.ctaContact}>
-                  Need a Developer?
-                  <BiMailSend
-                    className="animate__animated animate__pulse animate__fast animate__infinite"
-                    style={{ fontSize: '30px', marginLeft: '5px' }}
-                  />
-                </Button>
-              </Link>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </Layout>
-  );
+					<Col lg="6" className={indexStyles.bioCol}>
+						<div className={indexStyles.heroBio}>
+							<h1>Hello, I'm Stelios</h1>
+							<p>
+								A motivated and reliable Software Developer seeking to
+								work and grow in a tech environment.
+							</p>
+							<Link to="/contact">
+								<Button className={indexStyles.ctaContact}>
+									Need a Developer?
+									<BiMailSend
+										className="animate__animated animate__pulse animate__fast animate__infinite"
+										style={{ fontSize: '30px', marginLeft: '5px' }}
+									/>
+								</Button>
+							</Link>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</Layout>
+	);
 };
 
 export default Index_Page;
